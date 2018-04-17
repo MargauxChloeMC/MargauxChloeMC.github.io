e31fa54599f91460d9a969c4e5f68918b2735f46
chloemnge <chloemange@live.com>
2018-04-17 19:17:14 +0200

    New css and html created:added new functions on buttons and put frames for all quizz questions.

--- Script.js
+++ Script.js
@@ -1,34 +1,48 @@
-var $slideLink = $("#slide-link"),
-    slideLinkText = $slideLink.find("span")[0];
+// var $slideLink = $("#slide-link"),
+//     slideLinkText = $slideLink.find("span")[0];
+//
+// $slideLink.on("mouseenter", linkOver);
+//
+// function linkOver() {
+//   TweenLite.to(slideLinkText, 0.3, {y: -25, ease: Cubic.easeIn, onComplete: function() {
+//     TweenLite.fromTo(slideLinkText, 0.3, {y: 25}, {y: 0, ease: Cubic.easeOut})
+//   }});
+// }
+//
+// // Import data
+// // Using d3js
+// d3.json('repartition_francais.json', function(err, data) {
+//   $('#cacao-data-1').text(JSON.stringify(data, null, 2));
+// })
+//
+// function showBlock(imageId) {
+//   document.getElementById(imageId).style.display = "block";
+//
+// }
+//
+// // Clicks on buttons by id
+// $("#button4").click(function(){
+//     $("#answer4").toggle();
+// });
+//
+// $("#button2").click(function(){
+//     $("#answer2").toggle();
+// });
+//
+// $("#button3").click(function(){
+//     $("#answer3").toggle();
+// });
 
-$slideLink.on("mouseenter", linkOver);
+function showBlock(imageId1,bt1,bt2,right_answer) {
+   document.getElementById(imageId1).style.display = 'block'; // show answer
+   document.getElementById(bt1).disabled=true; // desactivate other buttons
+   document.getElementById(bt2).disabled=true;
+   //document.getElementById(right_answer).background = #009933; // essayer de mettre en vert le bouton de la vraie réponse
+ }
 
-function linkOver() {
-  TweenLite.to(slideLinkText, 0.3, {y: -25, ease: Cubic.easeIn, onComplete: function() {
-    TweenLite.fromTo(slideLinkText, 0.3, {y: 25}, {y: 0, ease: Cubic.easeOut})
-  }});
-}
-
-// Import data
-// Using d3js
-d3.json('repartition_francais.json', function(err, data) {
-  $('#cacao-data-1').text(JSON.stringify(data, null, 2));
-})
-
-function showBlock(imageId) {
-  document.getElementById(imageId).style.display = "block";
-  
-}
-
-// Clicks on buttons by id
-$("#button4").click(function(){
-    $("#answer4").toggle();
-});
-
-$("#button2").click(function(){
-    $("#answer2").toggle();
-});
-
-$("#button3").click(function(){
-    $("#answer3").toggle();
-});
+ function showMap(answer1,answer2,answer3,answer4){
+   document.getElementById(answer1).style.display = 'block';// show map
+   document.getElementById(answer2).style.display = 'none';// hide map2
+   document.getElementById(answer3).style.display = 'none';
+   document.getElementById(answer4).style.display = 'none';
+ }
